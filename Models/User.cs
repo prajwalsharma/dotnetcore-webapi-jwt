787a9d5927cf9125ecc84fd43c805f80f81f2921
prjwl4 <prjwl4@gmail.com>
2021-10-02 16:31:03 +0530

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JWT_Authentication_NET_Core_Web_API_5._0.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
